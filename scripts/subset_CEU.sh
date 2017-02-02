3012a52ec4ab97f2fac97865a3e06c8bc852a9a4
tnphung <tnphung@ucla.edu>
2017-02-02 10:02:25 -0800

    change the list of 10 CEU individuals because some could not be found in the original vcf files

--- scripts/subset_CEU.sh
+++ scripts/subset_CEU.sh
@@ -15,4 +15,4 @@ export PERL5LIB=$PERL5LIB:/u/project/klohmuel/tanya_data/softwares/vcftools_perl
 chrNum=${SGE_TASK_ID}
 vcf='/u/project/klohmuel/1000genomes/vcfs_zipped/ALL.chr'${chrNum}'.phase3_shapeit2_mvncall_integrated_v5a.20130502.genotypes.vcf.gz'
 outfile='/u/scratch/p/phung428/AnnabelProject/1000GVCF_subset/CEU/chr'${chrNum}'_10CEU.vcf'
-/u/project/klohmuel/tanya_data/softwares/vcftools_perl/src/perl/vcf-subset -c NA06984,NA06985,NA06986,NA06989,NA06993,NA06994,NA07000,NA07022,NA07031,NA07034 ${vcf} > ${outfile}
+/u/project/klohmuel/tanya_data/softwares/vcftools_perl/src/perl/vcf-subset -c NA06984,NA06985,NA06986,NA06989,NA06994,NA07000,NA07037,NA07051,NA07056,NA07347 ${vcf} > ${outfile}
