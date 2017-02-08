4cfe0cc92690ab74e88b8e09203015ccd49fb5e3
tnphung <tnphung@ucla.edu>
2017-02-08 10:42:24 -0800

    Add initial script

---
+++ scripts/vcftools_ld_CEU.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#$ -cwd
+#$ -V
+#$ -N CEUld
+#$ -l h_data=2G,time=12:00:00
+#$ -M eplau
+#$ -m ea
+#$ -t 1-22:1
+
+. /u/local/Modules/default/init/modules.sh
+module load vcftools
+
+chrNum=${SGE_TASK_ID}
+inVCF='/u/scratch/p/phung428/AnnabelProject/1000GVCF_subset_rmHomozygous_rmSingletons_pass/CEU/chr'${chrNum}'_10CEU_rmHomozygous_rmSingletons_pass.vcf'
+out='/u/scratch/p/phung428/AnnabelProject/LD/CEU/chr'${chrNum}'_LD'
+
+vcftools --vcf ${inVCF} --hap-r2 --ld-window-bp 100000 --out ${out}
