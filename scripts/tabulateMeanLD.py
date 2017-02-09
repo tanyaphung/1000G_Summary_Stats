9df8149cdf5128bfee4fca86a9dafea16eb8b6b2
tnphung <tnphung@ucla.edu>
2017-02-08 17:07:32 -0800

    Working version of script to tabulate rsquared and total SNPs across 22 autosomes

---
+++ scripts/tabulateMeanLD.py
@@ -0,0 +1,30 @@
+import os
+import sys
+
+bins_sumRSquare_dict = {}
+bins_totalSNPs_dict = {}
+
+with open("/u/project/klohmuel/tanya_data/1000G_Summary_Stats/results/LD/CHB/bins_range.txt", "r") as f:
+	for line in f:
+		line = line.rstrip("\n")
+		line = line.split("\t")
+		bins_sumRSquare_dict[(float(line[0]), float(line[1]))] = []
+		bins_totalSNPs_dict[(float(line[0]), float(line[1]))] = []
+
+for i in range(1, 23):
+	file = "/u/project/klohmuel/tanya_data/1000G_Summary_Stats/results/LD/CHB/chr%d_LD_100bins.txt" % i
+	with open(file, "r") as f:
+		for line in f:
+			line = line.rstrip("\n")
+			line = line.split("\t")
+			bins_sumRSquare_dict[(float(line[0]), float(line[1]))].append(float(line[2]))
+			bins_totalSNPs_dict[(float(line[0]), float(line[1]))].append(float(line[3]))
+
+bins_avg_sumRSquared = {}
+for bin in bins_sumRSquare_dict:
+	rSquaredAvg = float(sum(bins_sumRSquare_dict[bin]))/sum(bins_totalSNPs_dict[bin])
+	bins_avg_sumRSquared[bin] = rSquaredAvg
+
+for k, v in sorted(bins_avg_sumRSquared.items()):
+	toprint = [str(k[0]), str(k[1]), str(v)]
+	print"\t".join(x for x in toprint)
