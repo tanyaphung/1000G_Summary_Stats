86ea9fc8fa0d4ddfaafb2cca8b6d5e2c03b61266
tnphung <tnphung@ucla.edu>
2017-02-08 17:05:28 -0800

    Working version of script used to compute rsquared using VCFtools

--- scripts/vcftools_ld_YRI.sh
+++ scripts/vcftools_ld_YRI.sh
@@ -2,7 +2,7 @@
 #$ -cwd
 #$ -V
 #$ -N YRIld
-#$ -l h_data=4G,time=24:00:00
+#$ -l h_data=2G,time=12:00:00
 #$ -M eplau
 #$ -m ea
 #$ -t 1-22:1
@@ -10,8 +10,8 @@
 . /u/local/Modules/default/init/modules.sh
 module load vcftools
 
-chrNum=0${SGE_TASK_ID}
-inVCF='/u/scratch/p/phung428/AnnabelProject/1000GVCF_subset_rmHomozygous/YRI/chr'${chrNum}'_10YRI_rmHomozygous.vcf'
-out='/u/scratch/p/phung428/AnnabelProject/LD/YRI/chr'${chrNum}'_LD.txt'
+chrNum=${SGE_TASK_ID}
+inVCF='/u/scratch/p/phung428/AnnabelProject/1000GVCF_subset_rmHomozygous_rmSingletons_pass/YRI/chr'${chrNum}'_10YRI_rmHomozygous_rmSingletons_pass.vcf'
+out='/u/scratch/p/phung428/AnnabelProject/LD/YRI/chr'${chrNum}'_LD'
 
-vcftools --vcf ${inVCF} --hap-r2 --ld-window-bp 100000 > $out
+vcftools --vcf ${inVCF} --hap-r2 --ld-window-bp 100000 --out ${out}
